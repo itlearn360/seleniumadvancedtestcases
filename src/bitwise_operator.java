c1a4d69348594d11c9f6749732ebb056e45a6d0d
itlearn360 <training@itlearn360.com>
2018-07-17 18:47:11 +0530

    Changed

--- src/bitwise_operator.java
+++ src/bitwise_operator.java
@@ -5,7 +5,7 @@
 	    	 public static void main(String args[])
 
 	    	    {
-
+	    		 System.out.print("differnce");
 	    	        Thread t = Thread.currentThread();
 	    	        System.out.println(t.getPriority());
 	    	        System.out.print("differnce");
